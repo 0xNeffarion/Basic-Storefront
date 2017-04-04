cf78f43f9a28503848a18414a55facb6eae527a8
AndreMP97 <r.quente@hotmail.com>
2017-04-04 10:43:10 +0100

    Add files via upload
    Menu para a lista de compras

--- project/src/client/main.c
+++ project/src/client/main.c
@@ -2,13 +2,15 @@
 
 void sair();
 void menu();
+void actions(const int val);
 void saldo();
 void actionsaldo(const int opt);
-void actions(const int val);
+void lista();
+void actionslista(const int opt);
 
 void sair(){
   clearScreen();
-  printf("Saindo do cliente...\n");
+  printf("Saindo... Volte sempre!\n");
   usleep(1000*1500); //1,5s
   exit(0);
 }
@@ -29,6 +31,7 @@ void actions(const int val){
       break;
     }
     case 2:{
+      lista();
       break;
     }
     case 3:{
@@ -51,6 +54,7 @@ void actions(const int val){
 }
 
 void saldo(){
+  //Criar variável na struct
   int opt=0;
   clearScreen();
   ePrint(COLOR_YELLOW "** Saldo **" COLOR_RESET "\n");
@@ -66,6 +70,7 @@ void actionsaldo(const int opt) {
   int add;
   switch(opt){
   case 1:{
+    //Falta aceder à struct para obter o saldo do user
     clearScreen();
     ePrint("O seu saldo atual é: teste euros\n");
     usleep(1000*2000);
@@ -91,9 +96,70 @@ void actionsaldo(const int opt) {
   }
 }
 
+void lista(){
+  //Criar variável na struct
+  int opt=0;
+  clearScreen();
+  ePrint(COLOR_YELLOW "** Lista **" COLOR_RESET "\n");
+  ePrint(COLOR_CYAN "1)" COLOR_RESET " Criar Lista de Compras.\n");
+  ePrint(COLOR_CYAN "2)" COLOR_RESET " Consultar Lista de Compras.\n");
+  ePrint(COLOR_CYAN "3)" COLOR_RESET " Adicionar produtos à Lista de Compras.\n");
+  ePrint(COLOR_CYAN "4)" COLOR_RESET " Voltar ao menu inicial.\n");
+  ePrint("Insira a opção desejada(1-4):\n");
+  scanf("%d",&opt);
+  actionslista(opt);
+}
+
+void actionslista(const int opt){
+  switch(opt){
+  case 1:{
+    //aceder à lista na struct
+    clearScreen();
+    ePrint("Adicione os códigos dos produtos à sua lista:\n");
+    ePrint("Pressine 0 quando terminar de adicionar produtos à sua lista!\n");
+    int items=1;
+    for(int i=0;i<items;i++) {
+      int flag;
+      scanf("%d",&flag);
+      if(flag!=0) {
+	//adicionar na struct
+      }
+      else
+	break;
+      items++;
+    }
+    lista();
+    break;
+  }
+  case 2:{
+    //aceder à lista na struct
+    clearScreen();
+    ePrint("A sua lista é composta pelos seguintes produtos:");
+    printf("\n");
+    usleep(1000*2000);
+    lista();
+    break;
+  }
+  case 3:{
+    clearScreen();
+    actionslista(1);
+    break;
+  }
+  case 4:{
+    break;
+  } 
+  default:{
+    ePrint(COLOR_RED "ERRO!" COLOR_RESET " Opção inválida! Escolha entre a opção 1 a 3!\n");
+    scanf("%d",&opt);
+    actionslista(opt);
+    break;
+  }
+  }
+}
+
 int main(){
   int input=1;
-  parseUsers();
+  //parseUsers();
   do{
     clearScreen();
     menu();
