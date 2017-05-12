51cd3110e6c4d95cc8319d49c9b2866a26a49a6e
AndreMP97 <r.quente@hotmail.com>
2017-05-12 18:18:02 +0100

    Warnings

--- project/src/client/estatisticas.h
+++ project/src/client/estatisticas.h
@@ -7,15 +7,16 @@ void estatisticas(const int id){
 	int opt = 0;
 
 	clearScreen();
-	ePrint(COLOR_YELLOW "** Estatísticas **" COLOR_RESET "\n");
-	ePrint(COLOR_CYAN "1)" COLOR_RESET " Quantidade total de produtos comprados.\n");
-	ePrint(COLOR_CYAN "2)" COLOR_RESET " Quantidade comprada de um determinado produto.\n");
-	ePrint(COLOR_CYAN "3)" COLOR_RESET " Valor total gasto num determinado produto.\n");
-	ePrint(COLOR_CYAN "4)" COLOR_RESET " Valor total gasto.\n");
-	ePrint(COLOR_CYAN "5)" COLOR_RESET " Voltar ao menu inicial.\n");
+	ePrint(COLOR_YELLOW " +++ Estatísticas +++ " COLOR_RESET "\n");
+	ePrint(COLOR_CYAN "[1] ‣" COLOR_RESET " Quantidade total de produtos comprados.\n");
+	ePrint(COLOR_CYAN "[2] ‣" COLOR_RESET " Quantidade comprada de um determinado produto.\n");
+	ePrint(COLOR_CYAN "[3] ‣" COLOR_RESET " Valor total gasto num determinado produto.\n");
+	ePrint(COLOR_CYAN "[4] ‣" COLOR_RESET " Valor total gasto.\n");
+	ePrint(COLOR_MAGENTA "[5] ‣" COLOR_RESET " Voltar ao menu inicial.\n\n");
 	ePrint("Insira a opção desejada(1-5): ");
-	scanf("%d", &opt);
-	actionsestatisticas(opt,id);
+	if (scanf("%d", &opt) > 0) {
+	        actionsestatisticas(opt,id);
+	}
 }
 
 void actionsestatisticas(const int opt, const int id){
@@ -25,6 +26,7 @@ void actionsestatisticas(const int opt, const int id){
 	case 1: {
 		clearScreen();
 		if (clientstats[sid].totalp>0) {
+		  ePrint(COLOR_YELLOW " +++ Quantidade total de produtos comprados: +++" COLOR_RESET "\n");
 		  printf("Comprou %d produtos.\n",clientstats[sid].totalp);
 		}
 		else
@@ -37,15 +39,24 @@ void actionsestatisticas(const int opt, const int id){
 	case 2: {
 	        int prod=0, pos=-1;
 		clearScreen();
+		ePrint(COLOR_YELLOW " +++ Quantidade comprada de um determinado produto: +++" COLOR_RESET "\n");
 		ePrint("Insira o código do produto: ");
-		scanf("%d", &prod);
-	        pos=getItemPos(sid,prod);
-		clearScreen();
+		if (scanf("%d", &prod) > 0) {
+		  pos=getItemPos(sid,prod);
+		  clearScreen();
+		}
 		if (pos >= 0) {
+		  for(int i=0; i<10; i++) {
+		    printf("Quant: %d",clientstats[sid].quant[i]);
+		  }
+		  ePrint(COLOR_YELLOW " +++ Quantidade comprada de um determinado produto: +++" COLOR_RESET "\n");
 		  printf("Comprou %d vezes o produto %d.\n",clientstats[sid].quant[pos],prod);
 		}
-		else
+		else if (pos == -1 && prod > 0) { 
 		  printErr("Nunca comprou este produto!\n");
+		}
+		else
+		  printErr("Por favor insira um código de produto superior a 0!\n");
 		enterPrompt();
 		estatisticas(id);
 		break;
@@ -54,15 +65,21 @@ void actionsestatisticas(const int opt, const int id){
 	case 3: {
 	        int prod=0, pos=-1;
 		clearScreen();
+		ePrint(COLOR_YELLOW " +++ Valor total gasto num determinado produto: +++" COLOR_RESET "\n");
 		ePrint("Insira o código do produto: ");
-		scanf("%d", &prod);
-		pos=getItemPos(sid,prod);
-		clearScreen();
+		if (scanf("%d", &prod) > 0) {
+		  pos=getItemPos(sid,prod);
+		  clearScreen();
+		}
 		if (pos >= 0) {
+		  ePrint(COLOR_YELLOW " +++ Valor total gasto num determinado produto: +++" COLOR_RESET "\n");
 		  printf("Gastou %.2f€ no produto %d.\n",clientstats[sid].gasto[pos],prod);
 		}
-		else
+		else if (pos == -1 && prod > 0) { 
 		  printErr("Nunca comprou este produto!\n");
+		}
+		else
+		  printErr("Por favor insira um código de produto superior a 0!\n");
 	        enterPrompt();
 		estatisticas(id);
 		break;
@@ -71,10 +88,11 @@ void actionsestatisticas(const int opt, const int id){
 	case 4: {
 	        clearScreen();
 		if(clientstats[sid].total > 0) {
-		  printf("Total gasto: %.2f€\n",clientstats[sid].total);
+		  ePrint(COLOR_YELLOW " +++ Valor total gasto: +++" COLOR_RESET);
+		  printf("%.2f€\n",clientstats[sid].total);
 		}
 		else
-		  printf("Ainda não gastou dinheiro na nossa loja!\n");
+		  printErr("Ainda não gastou dinheiro na nossa loja!\n");
 		enterPrompt();
 		estatisticas(id);
 	        break;
